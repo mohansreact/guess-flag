d67e4ac7244f368a585eea9076c8cf06d5131693
mohankumar <mohankumar73@hotmail.com>
2019-03-27 13:56:16 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import CountryGame from './CountryGame';
+import worldImg from './Worldmap.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div className="flag-app">
+        <header 
+          className="title-header"
+          style={{backgroundImage: `url(${worldImg})`}}
           >
-            Learn React
-          </a>
+          <h1 className="title-text">Guess the flag</h1>
         </header>
+        <CountryGame />
       </div>
     );
   }
